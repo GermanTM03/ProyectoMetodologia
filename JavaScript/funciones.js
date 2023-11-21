c5ecfcb7175d4bcf5bbbafefee9d32fea90a42fa
GermanTM03 <german500tm@gmail.com>
2023-11-21 18:44:27 -0500

    Mi commit

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -1,3 +1,14 @@
+document.getElementById('empresaLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Previene el comportamiento por defecto del enlace
+    var opciones = document.getElementById('opcionesEmpresa');
+    if (opciones.style.display === 'none' || opciones.style.display === '') {
+        opciones.style.display = 'flex';
+    } else {
+        opciones.style.display = 'none';
+    }
+});
+
+
 function toggleIcon() {
     var queryInput = document.getElementById('query');
     var searchIcon = document.getElementById('search-icon');
